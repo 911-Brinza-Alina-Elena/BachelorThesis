aa7e34733798131e053a3d46ca2619469b602c6d
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2023-06-08 01:05:50 +0300

    maybe now my token won't expire every hour

--- app-server/api/config.py
+++ app-server/api/config.py
@@ -11,7 +11,7 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', None)
     if not JWT_SECRET_KEY:
         JWT_SECRET_KEY = ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DB_ENGINE = os.getenv('DB_ENGINE')
     DB_USERNAME = os.getenv('DB_USERNAME')
